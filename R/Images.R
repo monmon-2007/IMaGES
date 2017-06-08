bb687b77f2f53ad7140cc0d353427882b40c3b93
monmon-2007 <minamonir11@hotmail.com>
2017-06-08 14:41:36 -0400

    added discount and took care of non data files

--- R/Images.R
+++ R/Images.R
@@ -1,8 +1,8 @@
-Images<-function(filepath)
+Images<-function(filepath,discount)
 {
     .jaddLibrary('im', 'inst/java/Images.jar')
     .jaddClassPath('inst/java/Images.jar')
     str<- .jnew('edu/cmu/tetrad/cmd/Images')
-    .jcall(str, 'Ljava/lang/String;', 'Images',filepath)
+    .jcall(str, 'Ljava/lang/String;', 'Images',filepath,discount)
 }
 
